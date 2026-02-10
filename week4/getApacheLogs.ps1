c1cf965ccbc4af5f9cd766baa16bf1aef11fb02f
sean <sean.mcmurray@mymail.champlain.edu>
2026-02-10 16:00:04 -0500

    week 4 assignment

---
+++ week4/getApacheLogs.ps1
@@ -0,0 +1,20 @@
+﻿function getApacheLogs(){
+    $logs = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+    for ($i=0; $i -lt $logs.Count; $i++){
+        $log = $logs[$i] -split " ";
+        if($log[0] -ilike "10.*"){
+            $tableRecords += [pscustomobject]@{
+            "IP" = $log[0]
+            "Time" = $log[3].Trim('[')
+            "Method" = $log[5].Trim('"')
+            "Page" = $log[6]
+            "Protocol" = $log[7].Trim('"')
+            "Response" = $log[8].Trim('"')
+            "Referer" = if ($log.Count -ge 11) { $log[10].Trim('"') } else { "-".Trim('"') }
+            }
+        }
+    }
+
+    return $tableRecords
+}
---
+++ week4/main.ps1
@@ -0,0 +1,13 @@
+﻿.(Join-Path $PSScriptRoot getApacheLogs.ps1)
+
+clear
+
+$tableRecords = getApacheLogs
+
+Write-Host ($tableRecords | Format-Table -AutoSize | Out-String)
+
+Write-Host ($tableRecords | Where-Object {
+                                          $_.Page -like "/page2*" -and
+                                          $_.Referer -like "*index*"
+                                          }`
+                                          |Format-Table | Out-String)
