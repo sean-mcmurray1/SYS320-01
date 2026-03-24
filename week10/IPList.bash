a9bd041a752cd1da956fc930aaf7688bc8e332c8
sean <sean.mcmurray@mymail.champlain.edu>
2026-03-24 19:16:50 -0400

    week10

---
+++ week10/IPList.bash
@@ -0,0 +1,18 @@
+#!bin/bash
+
+# Usage: Bash IPList.bash 10.0.17
+[ $# -ne 1 ] && echo "Usage: $0 <Prefix>" && exit 1
+# Prefix is the first input taken
+prefix=$1
+
+#verify input length
+[ ${#prefix} -lt 5 ] && \
+printf "Prefix length is too short\nPrefix example: 10.0.17\n" && \
+exit 1
+
+for i in {1..254}
+do
+	ping -c 1 "$prefix.$i" \
+	| grep 'bytes from' \
+	| grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}'
+done
